4474cbbd06e97d99f5992d3bc818efe46704ec8c
Lynn Zenn <1zenn0@proton.me>
2023-05-10 06:40:31 +0200

    public: add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
 import clsx from "clsx";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "FireNostr",
   description: "Nostr Firehose - Stream of Nostr Events",
+  keywords: "Nostr, social network, social media protocol",
+  manifest: "/site.webmanifest",
+  openGraph: {
+    type: "website",
+    title: "FireNostr",
+    description: "Stream of Nostr Events",
+  },
 };
 
 export default function RootLayout({
