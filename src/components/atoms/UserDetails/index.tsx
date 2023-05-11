459d816c0025f913e3d5f2d63cc86a2385c60b20
Lynn Zenn <1zenn0@proton.me>
2023-05-11 05:27:21 +0200

    userdetails: make npub into "nostr:" url

--- src/components/atoms/UserDetails/index.tsx
+++ src/components/atoms/UserDetails/index.tsx
@@ -7,14 +7,20 @@ export type User = {
   identifier: string;
 };
 
-const UserDetails: FC<User> = ({ name, identifier, sources }) => (
-  <div className="flex flex-col max-w-full text-base-content overflow-clip truncate">
-    <p className="mb-1 text-sm truncate">{name}</p>
-    <p className="text-xs opacity-80 truncate">
-      {nip19.npubEncode(identifier)}
-    </p>
-    <p className="text-xs opacity-80 truncate">{sources[0] || ""}</p>
-  </div>
-);
+const UserDetails: FC<User> = ({ name, identifier, sources }) => {
+  const npub = nip19.npubEncode(identifier);
+  return (
+    <div className="flex flex-col max-w-full text-base-content overflow-clip truncate">
+      <p className="mb-1 text-sm truncate">{name}</p>
+      <p className="text-xs opacity-80 truncate">
+        {/* Would do nprofile, but iris doesn't support it. */}
+        <a target="_blank" href={"nostr:" + npub}>
+          {npub}
+        </a>
+      </p>
+      <p className="text-xs opacity-80 truncate">{sources[0] || ""}</p>
+    </div>
+  );
+};
 
 export default UserDetails;
